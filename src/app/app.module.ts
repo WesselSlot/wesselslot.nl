4edadbf53939f6e1fadeb70bf4e243d92f71db20
w.slot <wesselslot1@gmail.com>
2019-07-30 22:47:14 +0200

    Started to implement the blog page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
